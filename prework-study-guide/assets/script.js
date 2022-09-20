056c3dbff5aec1e98b735ce1c6f534910c0b3260
v1ct0r14m <tori.a.g.mota@gmail.com>
2022-09-20 01:36:54 -0500

    for loop and more notes and small css additons of #pic

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,22 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+
+
+
+/*
+if (topic === 'HTML') {
+    console.log("LET'S STUDY HTML!");
+} else if (topic === 'CSS') { 
+    console.log("LET'S STUDY CSS!");
+} else if (topic === 'Git') {
+    console.log("LET'S STUDY GIT!");
+} else if (topic === "JavaScript") {
+    console.log("LET'S STUDY JAVASCRIPT!")
+} else {
+    console.log('please try again :^/')
+}
+*/
